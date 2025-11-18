797c501e45e7c83dcb21444664cb1f37da8ebb38
IMANI <kiplagatfaith88@gmail.com>
2025-11-18 19:23:19 +0300

    fixed a few errors

--- Frontend/frontend/vite.config.js
+++ Frontend/frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-  ,tailwindcss(),
-],
+    tailwindcss(), 
+  ],
+  base: './', 
 })
